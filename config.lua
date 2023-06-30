b28b310707df021646b51d3a27f2bcee80989c17
Ronny Riggs <100510537+sledgehamm3r@users.noreply.github.com>
2023-06-30 15:18:33 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,37 @@
+--# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+--# ███████ ██   ██  █████  ███    ███ ███████ ██    ██ #
+--# ██      ██   ██ ██   ██ ████  ████ ██      ██    ██ #
+--# ███████ ███████ ███████ ██ ████ ██ █████   ██    ██ #
+--#      ██ ██   ██ ██   ██ ██  ██  ██ ██       ██  ██  #
+--# ███████ ██   ██ ██   ██ ██      ██ ███████   ████   #
+--#                                                     # 
+--#     Owner: sledge_hamm3r                            #
+--#     Discord: https://discord.gg/vVnxwGfg            #
+--#     Website: https://shamev.de                      #
+--# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+Config = {}
+
+-- Anschnallton abspielen Ja/Nein
+Config.Sounds = true
+Config.LoopSound = false
+Config.Volume = 0.8
+-- Min: 0.0 Max: 1.0
+
+-- Benachrichtungen An/Aus
+Config.Notification = true
+Config.Strings = {seatbelt_on = 'Sicherheitsgurt: ~g~angeschnallt', seatbelt_off = 'Sicherheitsgurt: ~r~abgeschnallt'}
+
+-- Warnbild bei nichtanschnallen An/Aus
+Config.Blinker = true
+
+-- Taste für den Gurt (docs.fivem.net/docs/game-references/controls)
+Config.Control = 311
+
+-- Geschwindigkeit, ab wann man aus dem Auto fällt, wenn man nicht angeschnallt ist.
+Config.Speed = 50.0
+
+-- Ab wieviel KM/H soll das Warnbild erscheinen
+Config.AlarmOnlySpeed = true
+Config.AlarmSpeed = 20
+
---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+--# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+--# ███████ ██   ██  █████  ███    ███ ███████ ██    ██ #
+--# ██      ██   ██ ██   ██ ████  ████ ██      ██    ██ #
+--# ███████ ███████ ███████ ██ ████ ██ █████   ██    ██ #
+--#      ██ ██   ██ ██   ██ ██  ██  ██ ██       ██  ██  #
+--# ███████ ██   ██ ██   ██ ██      ██ ███████   ████   #
+--#                                                     # 
+--#     Owner: sledge_hamm3r                            #
+--#     Discord: https://discord.gg/vVnxwGfg            #
+--#     Website: https://shamev.de                      #
+--# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+fx_version 'adamant'
+
+game 'gta5'
+author 'sledge_hamm3r / ShameV'
+description 'NPC aus'
+version '1.0.0'
+
+client_scripts {
+	'client.lua',
+	'config.lua'
+}
+
+ui_page 'html/ui.html'
+
+files {
+	'html/*',
+	'html/img/seatbelt.png',
+	'html/sounds/*.ogg'
+}
